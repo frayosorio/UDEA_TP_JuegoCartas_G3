c3a0e080cacb90b27e0a5a799a2f568e23823136
fray osorio <frayosorio@gmail.com>
2025-09-03 17:56:27 -0500

    Se completa la funcionalidad de VERIFICACION DE GRUPOS de las cartas de los jugadores

--- src/FrmJuego.java
+++ src/FrmJuego.java
@@ -1,7 +1,10 @@
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 import javax.swing.WindowConstants;
@@ -9,6 +12,8 @@ import javax.swing.WindowConstants;
 public class FrmJuego extends JFrame {
 
     private JPanel pnlJugador1, pnlJugador2;
+    private Jugador jugador1, jugador2;
+    private JTabbedPane tpJugadores;
 
     public FrmJuego() {
 
@@ -26,18 +31,63 @@ public class FrmJuego extends JFrame {
         getContentPane().add(btnVerificar);
 
         pnlJugador1 = new JPanel();
+        pnlJugador1.setLayout(null);
         pnlJugador1.setBackground(new Color(150, 255, 50));
 
-        pnlJugador2= new JPanel();
+        pnlJugador2 = new JPanel();
+        pnlJugador2.setLayout(null);
         pnlJugador2.setBackground(new Color(0, 255, 255));
 
-        JTabbedPane tpJugadores = new JTabbedPane();
+        tpJugadores = new JTabbedPane();
         tpJugadores.addTab("Martín Estrada Contreras", pnlJugador1);
         tpJugadores.addTab("Raúl Vidal", pnlJugador2);
 
         tpJugadores.setBounds(10, 40, 550, 200);
         getContentPane().add(tpJugadores);
 
+        // Agregar los eventos
+        btnRepartir.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                repartir();
+            }
+
+        });
+
+        btnVerificar.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                verificar();
+            }
+
+        });
+
+        jugador1 = new Jugador();
+        jugador2 = new Jugador();
+
+    }
+
+    private void repartir() {
+        // repartir cartas
+        jugador1.repartir();
+        jugador2.repartir();
+
+        // mostrar las cartas de cada jugador
+        jugador1.mostrar(pnlJugador1);
+        jugador2.mostrar(pnlJugador2);
+    }
+
+    private void verificar() {
+        switch (tpJugadores.getSelectedIndex()) {
+            case 0:
+                JOptionPane.showMessageDialog(null, jugador1.getGrupos());
+                break;
+            case 1:
+                JOptionPane.showMessageDialog(null, jugador2.getGrupos());
+                break;
+        }
     }
 
 }
---
+++ src/Jugador.java
@@ -0,0 +1,68 @@
+import java.util.Random;
+
+import javax.swing.JPanel;
+
+public class Jugador {
+
+    private final int TOTAL_CARTAS = 10;
+    private final int MARGEN = 10;
+    private final int SEPARACION = 40;
+    private Carta[] cartas = new Carta[TOTAL_CARTAS];
+    private Random r = new Random();
+
+    public void repartir() {
+        for (int i = 0; i < TOTAL_CARTAS; i++) {
+            cartas[i] = new Carta(r);
+        }
+    }
+
+    public void mostrar(JPanel pnl) {
+        pnl.removeAll();
+        /*
+         * for (int i = 0; i < TOTAL_CARTAS; i++) {
+         * Carta carta=cartas[i];
+         * carta.mostrar(pnl, , );
+         * }
+         */
+        int posicion = MARGEN + TOTAL_CARTAS * SEPARACION;
+        for (Carta carta : cartas) {
+            carta.mostrar(pnl, posicion, MARGEN);
+            posicion -= SEPARACION;
+        }
+        pnl.repaint();
+    }
+
+    public String getGrupos() {
+        String resultado = "No se encontraron grupos";
+        // calcular los contadres de las cartas
+        int[] contadores = new int[NombreCarta.values().length];
+        for (Carta carta : cartas) {
+            contadores[carta.getNombre().ordinal()]++;
+        }
+
+        // validar si hubo grupos
+        boolean hayGrupos = false;
+        for (int contador : contadores) {
+            if (contador >= 2) {
+                hayGrupos = true;
+                break;
+            }
+        }
+
+        // obtener los grupos
+        if (hayGrupos) {
+            resultado = "Se hallaron los siguientes grupos:\n";
+
+            for (int i = 0; i < contadores.length; i++) {
+                int contador = contadores[i];
+                if (contador >= 2) {
+                    resultado += Grupo.values()[contador] + " de " + NombreCarta.values()[i] + "\n";
+                }
+
+            }
+        }
+
+        return resultado;
+    }
+
+}
